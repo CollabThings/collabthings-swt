c7b75fe967c103e39ebd9dbe98ab80115a1a97a1
jeukku <juuso@vilmunen.net>
2014-01-04 04:42:02 +0200

    new version of the launcher.

--- src/main/java/org/libraryofthings/swt/AppLauncher.java
+++ src/main/java/org/libraryofthings/swt/AppLauncher.java
@@ -5,6 +5,11 @@ public class AppLauncher {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
