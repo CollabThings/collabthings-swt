54799c81c922bd7e9ec08ea4d83fa0d857faee8e
Juuso Vilmunen <juuso@vilmunen.net>
2014-05-28 16:24:48 +0300

    Setting proxy before launching.

--- src/main/java/org/libraryofthings/swt/AppLauncher.java
+++ src/main/java/org/libraryofthings/swt/AppLauncher.java
@@ -1,6 +1,12 @@
 package org.libraryofthings.swt;
 
+import java.net.InetSocketAddress;
 import java.net.MalformedURLException;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Iterator;
+import java.util.List;
 
 import org.libraryofthings.LLog;
 import org.libraryofthings.swt.app.LOTApp;
@@ -8,7 +14,8 @@ import org.libraryofthings.swt.app.LOTApp;
 public final class AppLauncher {
 	private LOTApp app;
 	private LoginWindow loginwindow;
-
+	private LLog log = LLog.getLogger(this);
+	
 	private void openWindow() {
 		AppWindow w = new AppWindow(app);
 		w.open();
@@ -16,6 +23,8 @@ public final class AppLauncher {
 
 	private void launch() throws MalformedURLException {
 		LLog.getLogger(this).info("Launching " + this);
+		setProxy();
+		//
 		app = new LOTApp();
 		try {
 			loginwindow = new LoginWindow(app);
@@ -29,6 +38,35 @@ public final class AppLauncher {
 		} finally {
 			app.close();
 		}
+	}
+	
+	private void setProxy() {
+		System.setProperty("java.net.useSystemProxies", "true");
+		log.info("detecting proxies");
+		List l = null;
+		try {
+		    l = ProxySelector.getDefault().select(new URI("http://waazdoh.com"));
+		} 
+		catch (URISyntaxException e) {
+		    e.printStackTrace();
+		}
+		if (l != null) {
+		    for (Iterator iter = l.iterator(); iter.hasNext();) {
+		    	java.net.Proxy proxy = (java.net.Proxy) iter.next();
+		    	log.info("proxy hostname : " + proxy.type());
+
+		    	InetSocketAddress addr = (InetSocketAddress) proxy.address();
+
+		    	if (addr == null) {
+		    		log.info("No Proxy");
+		    	} else {
+		    		log.info("proxy hostname : " + addr.getHostName());
+		    		System.setProperty("http.proxyHost", addr.getHostName());
+		    		log.info("proxy port : " + addr.getPort());
+		    		System.setProperty("http.proxyPort", Integer.toString(addr.getPort()));
+		    	}
+		    }
+		}		
 	}
 
 	public static void main(String[] args) throws MalformedURLException {
