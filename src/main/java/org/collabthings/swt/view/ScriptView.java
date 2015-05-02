bfb3b716f64b99cb9ce84ad25b416a841b05daa2
Juuso Vilmunen <juuso@vilmunen.net>
2015-05-02 16:41:18 +0300

    Menuitem to save script

--- src/main/java/org/collabthings/swt/view/ScriptView.java
+++ src/main/java/org/collabthings/swt/view/ScriptView.java
@@ -1,5 +1,7 @@
 package org.collabthings.swt.view;
 
+import java.util.Date;
+
 import org.collabthings.model.LOTScript;
 import org.collabthings.swt.AppWindow;
 import org.collabthings.swt.LOTAppControl;
@@ -8,6 +10,8 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.events.KeyAdapter;
 import org.eclipse.swt.events.KeyEvent;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -22,22 +26,26 @@ public class ScriptView extends Composite implements LOTAppControl {
 	private Text scripttext;
 	private LOTScript script;
 	private Text bottomtext;
-	private String shouldsave;
-	private Thread savingthread;
 
-	public ScriptView(Composite c, LOTApp app, AppWindow appWindow, LOTScript script) {
+	private LOTApp app;
+
+	public ScriptView(Composite c, LOTApp app, AppWindow appWindow,
+			LOTScript script) {
 		super(c, SWT.NONE);
 		this.window = appWindow;
+		this.app = app;
+
 		setLayout(new GridLayout(1, false));
 		this.script = script;
 
 		SashForm sashForm = new SashForm(this, SWT.VERTICAL);
-		GridData gd_sashForm = new GridData(SWT.FILL, SWT.FILL, true, true, 1, 1);
+		GridData gd_sashForm = new GridData(SWT.FILL, SWT.FILL, true, true, 1,
+				1);
 		gd_sashForm.heightHint = 200;
 		sashForm.setLayoutData(gd_sashForm);
 
-		scripttext = new Text(sashForm, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL | SWT.CANCEL
-				| SWT.MULTI);
+		scripttext = new Text(sashForm, SWT.BORDER | SWT.H_SCROLL
+				| SWT.V_SCROLL | SWT.CANCEL | SWT.MULTI);
 		scripttext.addKeyListener(new KeyAdapter() {
 			@Override
 			public void keyReleased(KeyEvent arg0) {
@@ -47,33 +55,18 @@ public class ScriptView extends Composite implements LOTAppControl {
 
 		scripttext.setText("" + script.getScript());
 
-		bottomtext = new Text(sashForm, SWT.BORDER | SWT.H_SCROLL | SWT.V_SCROLL | SWT.CANCEL
-				| SWT.MULTI);
+		bottomtext = new Text(sashForm, SWT.BORDER | SWT.H_SCROLL
+				| SWT.V_SCROLL | SWT.CANCEL | SWT.MULTI);
 		sashForm.setWeights(new int[] { 3, 1 });
 	}
 
 	protected synchronized void key(KeyEvent arg0) {
-		startSave();
+
 	}
 
-	private void startSave() {
-		shouldsave = this.scripttext.getText();
-		if (savingthread == null) {
-			savingthread = new Thread(() -> {
-				synchronized (savingthread) {
-					do {
-						shouldsave = null;
-						try {
-							savingthread.wait(500);
-						} catch (Exception e) {
-						}
-						save(shouldsave);
-					} while (shouldsave != null);
-					savingthread = null;
-				}
-			});
-			savingthread.start();
-		}
+	private synchronized void startSave() {
+		String sstring = this.scripttext.getText();
+		save(sstring);
 	}
 
 	@Override
@@ -88,15 +81,19 @@ public class ScriptView extends Composite implements LOTAppControl {
 
 	private void save(String sscripttext) {
 		if (sscripttext != null
-				&& (script.getScript() == null || !script.getScript().equals(sscripttext))) {
+				&& (script.getScript() == null || !script.getScript().equals(
+						sscripttext))) {
+
+			LOTScript s = this.app.getObjectFactory().getScript();
+			s.setScript(sscripttext);
 			getDisplay().asyncExec(() -> {
-				script.setScript(sscripttext);
-				boolean b = script.isOK();
-				if (!b) {
+				if (s.isOK()) {
+					script.setScript(sscripttext);
+					boolean b = script.isOK();
+					bottomtext.append("OK " + new Date() + "\n");
+				} else {
 					String error = script.getError();
 					bottomtext.append("ERROR " + error + "\n");
-				} else {
-					bottomtext.append("OK\n");
 				}
 			});
 		}
@@ -114,6 +111,15 @@ public class ScriptView extends Composite implements LOTAppControl {
 
 		Menu mscript = new Menu(miscripts);
 		miscripts.setMenu(mscript);
+
+		MenuItem msave = new MenuItem(mscript, SWT.NONE);
+		msave.setText("Save");
+		msave.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent arg0) {
+				startSave();
+			}
+		});
 
 		return miscripts;
 	}
